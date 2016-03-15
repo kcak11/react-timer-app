e15a7de3fbdc6b72632773e10ed6c34f3dfe1d25
Andrew Mead <andrewjosephmead@gmail.com>
2016-03-15 14:11:58 -0400

    Create clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,8 +1,13 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function () {
-    return <p>Countdown.jsx</p>;
+    return (
+      <div>
+        <Clock totalSeconds={129}/>
+      </div>
+    );
   }
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = {
       applicationStyles: 'app/styles/app.scss',
       Navigation: 'app/components/Navigation.jsx',
       Timer: 'app/components/Timer.jsx',
-      Countdown: 'app/components/Countdown.jsx'
+      Countdown: 'app/components/Countdown.jsx',
+      Clock: 'app/components/Clock.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
